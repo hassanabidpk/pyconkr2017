4d71ad8d9eb6e99194e70b1bccfd0b790e436a05
Hassan Abid <hassanabidpk89@gmail.com>
2017-08-05 05:45:55 +0900

    add until models and migrations

--- food/models.py
+++ food/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Restaurant(models.Model):
+       
+    name = models.CharField(max_length=200)
+    address = models.TextField()
+    photo = models.ImageField(upload_to="food/photos/", null=True, blank=True)
+    menu = models.TextField()
+    tags = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    writer = models.ForeignKey(User)
+
