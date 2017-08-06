02ea20d9cbd1967eae49ab213204c9a729e487a8
Hassan Abid <hassanabidpk89@gmail.com>
2017-08-06 15:52:26 +0900

    write unit tests

--- food/tests.py
+++ food/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from food.models import Restaurant
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class RestaurantTestCase(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+        writer = User.objects.create_user('test_user', \
+        'test@example.com', 'password1')
+        rest = Restaurant.objects.create(name="kfc", \
+        address="seoul", menu="burger", tags="burger", writer=writer)
+
+    def test_restaurant_has_name(self):
+        """Restaurant object is created Ok with a name """
+        rest = Restaurant.objects.get(name="kfc")
+        self.assertEqual(rest.name, "kfc")
+
+    def test_index_page(self):
+        "Check if new restaurant is added to index page"
+        response = self.client.get("/")
+        self.assertContains(response, "kfc")
+
