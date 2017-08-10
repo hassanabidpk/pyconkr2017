2767da90e06098ad64ca68b7d4c97c08c3bab90d
Hassan Abid <hassanabidpk89@gmail.com>
2017-08-10 13:02:29 +0900

    add url for static files

--- pyconlunch/urls.py
+++ pyconlunch/urls.py
@@ -22,3 +22,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('food.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
